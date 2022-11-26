8f9696c83f2c0ebca8e6367d96299cc25e68b816
Anna Haratova <black-isida@yandex.ru>
2022-11-26 23:20:23 +0300

    ПР4

---
+++ scripts/index.js
@@ -0,0 +1,38 @@
+
+let popup = document.querySelector('.popup');
+let editButton =  document.querySelector('.profile__edit-button');
+let closePopup = document.querySelector('.popup__close-button')
+
+function addPopup () {
+  popup.classList.add('popup__opend');
+}
+
+editButton.addEventListener('click', addPopup);
+
+function removePopup() {
+  popup.classList.remove('popup__opend');
+}
+
+closePopup.addEventListener('click', removePopup);
+
+
+let profileName = document.querySelector('#profile__name');
+let inputName = document.querySelector('#popup__input_name');
+
+inputName.value = profileName.textContent;
+
+let profileProfession = document.querySelector('#profile__profession');
+let inputProfession = document.querySelector('#popup__input_profession');
+
+inputProfession.value = profileProfession.textContent;
+
+let saveButton = document.querySelector('.popup__form');
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileProfession.textContent = inputProfession.value;
+  removePopup();
+}
+
+saveButton.addEventListener('click', formSubmitHandler);
